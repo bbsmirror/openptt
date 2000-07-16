e9d537213907af03079b24eed41bfc67805b9912
Chia-Hsing Yu <davidyu@users.sourceforge.net>
2000-07-16 01:09:57 +0000

    test commit

--- include/config.h
+++ include/config.h
@@ -20,7 +20,6 @@
 #define RELAYSERVERIP     "127.0.0.1"
 #endif
 
-
 /* 临ゼ俱瞶 */
 #define MAX_FRIEND        (256)          /* 更 cache ぇ程狟ね计ヘ */
 #define MAX_REJECT        (32)           /* 更 cache ぇ程胊计ヘ */

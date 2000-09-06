beaced57df1195dff80c277e8bbd55150ec47be7
Chia-Hsing Yu <davidyu@users.sourceforge.net>
2000-09-06 01:46:15 +0000

    fix bug

--- util/reaper.c
+++ util/reaper.c
@@ -20,8 +20,8 @@ int invalid(char *userid) {
     if(!isalpha(userid[0]))
 	return 1;
     
-    for(i = 1; i < IDLEN && *userid; i++, userid++)
-	if(!isalpha(*userid) || !isdigit(*userid))
+    for(i = 1, userid++; i < IDLEN && *userid; i++, userid++)
+	if(!isalpha(*userid) && !isdigit(*userid))
 	    return 1;
     return 0;
 }
@@ -30,21 +30,23 @@ int check(int n, userec_t *u) {
     time_t d;
     char buf[256];
     
-    if(invalid(u->userid))
-	syslog(LOG_ERR, "bad userid: %s", u->userid);
-    else {
-	d = now - u->lastlogin;
-	if((d > MAX_GUEST_LIFE && (u->userlevel & PERM_LOGINOK) == 0) ||
-	   (d > MAX_LIFE && (u->userlevel & PERM_XEMPT) == 0)) {
-	    /* expired */
-	    strcpy(buf, ctime(&u->lastlogin));
-	    strtok(buf, "\n");
-	    syslog(LOG_NOTICE, "kill user: %s %s", u->userid, buf);
-	    sprintf(buf, "mv home/%c/%s tmp/", u->userid[0], u->userid);
-	    if(system(buf))
-		syslog(LOG_ERR, "can't move user home: %s", u->userid);
-	    u->userid[0] = '\0';
-	    remove_from_uhash(n);
+    if(u->userid[0] == '\0') {
+	if(invalid(u->userid))
+	    syslog(LOG_ERR, "bad userid: %s", u->userid);
+	else {
+	    d = now - u->lastlogin;
+	    if((d > MAX_GUEST_LIFE && (u->userlevel & PERM_LOGINOK) == 0) ||
+	       (d > MAX_LIFE && (u->userlevel & PERM_XEMPT) == 0)) {
+		/* expired */
+		strcpy(buf, ctime(&u->lastlogin));
+		strtok(buf, "\n");
+		syslog(LOG_NOTICE, "kill user: %s %s", u->userid, buf);
+		sprintf(buf, "mv home/%c/%s tmp/", u->userid[0], u->userid);
+		if(system(buf))
+		    syslog(LOG_ERR, "can't move user home: %s", u->userid);
+		u->userid[0] = '\0';
+		remove_from_uhash(n);
+	    }
 	}
     }
     return 0;

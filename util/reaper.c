2f3e01142a67814a615ac2468b19747d7a2df009
Chia-Hsing Yu <davidyu@users.sourceforge.net>
2000-09-08 23:10:51 +0000

    remove_from_uhash()

--- util/reaper.c
+++ util/reaper.c
@@ -20,8 +20,8 @@ int invalid(char *userid) {
     if(!isalpha(userid[0]))
 	return 1;
     
-    for(i = 1, userid++; i < IDLEN && *userid; i++, userid++)
-	if(!isalpha(*userid) && !isdigit(*userid))
+    for(i = 1; i < IDLEN && userid[i]; i++)
+	if(!isalpha(userid[i]) && !isdigit(userid[i]))
 	    return 1;
     return 0;
 }
@@ -30,22 +30,27 @@ int check(int n, userec_t *u) {
     time_t d;
     char buf[256];
     
-    if(u->userid[0] == '\0') {
-	if(invalid(u->userid))
-	    syslog(LOG_ERR, "bad userid: %s", u->userid);
-	else {
+    if(u->userid[0] != '\0') {
+	if(invalid(u->userid)) {
+	    syslog(LOG_ERR, "bad userid(%d): %s", n, u->userid);
+	    u->userid[0] = '\0';
+	} else {
 	    d = now - u->lastlogin;
 	    if((d > MAX_GUEST_LIFE && (u->userlevel & PERM_LOGINOK) == 0) ||
 	       (d > MAX_LIFE && (u->userlevel & PERM_XEMPT) == 0)) {
 		/* expired */
+		int n;
+		
+		if((n = searchuser(u->userid)) != 0)
+		    remove_from_uhash(n - 1);
+		
 		strcpy(buf, ctime(&u->lastlogin));
 		strtok(buf, "\n");
-		syslog(LOG_NOTICE, "kill user: %s %s", u->userid, buf);
+		syslog(LOG_NOTICE, "kill user(%d): %s %s", n, u->userid, buf);
 		sprintf(buf, "mv home/%c/%s tmp/", u->userid[0], u->userid);
 		if(system(buf))
 		    syslog(LOG_ERR, "can't move user home: %s", u->userid);
 		u->userid[0] = '\0';
-		remove_from_uhash(n);
 	    }
 	}
     }

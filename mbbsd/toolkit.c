7065dce2c4b2515a56548af8ecedfad38c996e31
cvs2git <>
2001-06-17 12:58:30 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.9.2'.

---
+++ mbbsd/toolkit.c
@@ -0,0 +1,32 @@
+/* $Id$ */
+#include <ctype.h>
+#include <sys/types.h>
+#include "config.h"
+#include "pttstruct.h"
+
+unsigned StringHash(unsigned char *s) {
+    unsigned int v=0;
+    while(*s) {
+	v = (v << 8) | (v >> 24);
+	v ^= toupper(*s++);	/* note this is case insensitive */
+    }
+    return (v * 2654435769UL) >> (32 - HASH_BITS);
+}
+
+int IsNum(char *a, int n) {
+    int i;
+
+    for(i = 0; i < n; i++)
+	if (a[i] > '9' || a[i] < '0' )
+	    return 0;
+    return 1;
+}
+
+int IsSNum(char *a) {
+    int i;
+    
+    for(i = 0; a[i]; i++)
+	if(a[i] > '9' || a[i] < '0')
+	    return 0;
+    return 1;
+}

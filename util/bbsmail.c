cb879b0c168109e40665e1802df7e00bd8d9348c
Chia-Hsing Yu <davidyu@users.sourceforge.net>
2001-08-30 12:15:48 +0000

    MFC: Add Message-ID

--- util/bbsmail.c
+++ util/bbsmail.c
@@ -40,159 +40,120 @@ mailog(msg)
 }
 
 
-int
-mail2bbs(userid)
-    char *userid;
-{
+int mail2bbs(char *userid) {
     fileheader_t mymail;
-    char genbuf[256], title[80], sender[80], filename[80], *ip, *ptr;
+    char genbuf[256], title[TTLEN], sender[80], filename[80], *ip, *ptr;
+    char msgid[80];
     time_t tmp_time;
     struct stat st;
     FILE *fout;
 
-/* check if the userid is in our bbs now */
-    if (!searchuser(userid))
-    {
-	sprintf(genbuf, "BBS user <%s> not existed", userid);
+    /* check if the userid is in our bbs now */
+    if(!searchuser(userid)) {
+	snprintf(genbuf, sizeof(genbuf), "BBS user <%s> not existed", userid);
 	puts(genbuf);
 	mailog(genbuf);
 	return -1;//EX_NOUSER;
     }
-
-    sprintf(filename, BBSHOME "/home/%c/%s", userid[0], userid);
-
-    if (stat(filename, &st) == -1)
-    {
-	if (mkdir(filename, 0755) == -1)
-	{
+    
+    snprintf(filename, sizeof(filename), BBSHOME "/home/%c/%s", userid[0], userid);
+    
+    if(stat(filename, &st) == -1) {
+	if(mkdir(filename, 0755) == -1) {
 	    printf("mail box create error %s \n", filename);
 	    return -1;
 	}
-    }
-    else if (!(st.st_mode & S_IFDIR))
-    {
+    } else if(!(st.st_mode & S_IFDIR)) {
 	printf("mail box error\n");
 	return -1;
     }
-
+    
     printf("dir: %s\n", filename);
-
-/* allocate a file for the new mail */
-
+    
+    /* allocate a file for the new mail */
+    
     stampfile(filename, &mymail);
     printf("file: %s\n", filename);
-
-/* copy the stdin to the specified file */
-
-/* parse header */
-
-    while (fgets(genbuf, 255, stdin))
-    {
-	if (!strncmp(genbuf, "From", 4))
-	{
-	    if ((ip = strchr(genbuf, '<')) && (ptr = strrchr(ip, '>')))
-	    {
+    
+    /* copy the stdin to the specified file */
+    
+    /* parse header */
+    sender[0] = title[0] = msgid[0] = '\0';
+    while(fgets(genbuf, 255, stdin)) {
+	if(strncmp(genbuf, "From", 4) == 0) {
+	    if((ip = strchr(genbuf, '<')) && (ptr = strrchr(ip, '>'))) {
 		*ptr = '\0';
-		if (ip[-1] == ' ')
+		if(ip[-1] == ' ')
 		    ip[-1] = '\0';
 		ptr = (char *) strchr(genbuf, ' ');
-		while (*++ptr == ' ') ;
-		sprintf(sender, "%s (%s)", ip + 1, ptr);
-	    }
-	    else
-	    {
+		while(*++ptr == ' ');
+		snprintf(sender, sizeof(sender), "%s (%s)", ip + 1, ptr);
+	    } else {
 		strtok(genbuf, " \t\n\r");
-		strcpy(sender, (char *) strtok(NULL, " \t\n\r"));
+		strncpy(sender, strtok(NULL, " \t\n\r"), sizeof(sender));
+		sender[sizeof(sender) - 1] = '\0';
 	    }
-	    continue;
-	}
-	if (!strncmp(genbuf, "Subject: ", 9))
-	{
-	    strcpy(title, genbuf + 9);
-	    continue;
-	}
-	if (genbuf[0] == '\n')
+	} else if(strncmp(genbuf, "Subject: ", 9) == 0) {
+	    strncpy(title, genbuf + 9, sizeof(title));
+	    title[sizeof(title) - 1] = '\0';
+	} else if(strncmp(genbuf, "Message-ID: ", 12) == 0) {
+	    strncpy(msgid, genbuf + 12, sizeof(msgid));
+	    msgid[sizeof(msgid) - 1] = 0;
+	} else if(genbuf[0] == '\n')
 	    break;
     }
-
-    if ((ptr = strchr(sender, '\n')))
+    
+    if((ptr = strchr(sender, '\n')) != NULL)
 	*ptr = '\0';
-
-    if ((ptr = strchr(title, '\n')))
+    
+    if((ptr = strchr(title, '\n')) != NULL)
 	*ptr = '\0';
-
-    if (strchr(sender, '@') == NULL)	/* 由 local host 寄信 */
-    {
-	strcat(sender, "@" MYHOSTNAME);
+    
+    if((ptr = strchr(msgid, '\n')) != NULL)
+	*ptr = '\0';
+    
+    if((ptr = strchr(sender, '@')) == NULL) { /* 由 local host 寄信 */
+	strncpy(ptr, "@" MYHOSTNAME, sizeof(sender) - (ptr - sender));
+	sender[sizeof(sender) - 1] = '\0';
     }
-
+    
     time(&tmp_time);
-
-#ifdef HMM_USE_ANTI_SPAM
-    for (n = 0; notitle[n]; n++)
-	if (strstr(title, notitle[n]))
-	{
-	    sprintf(genbuf, "Title <%s> not accepted", title);
-	    puts(genbuf);
-	    mailog(genbuf);
-	    return -1;
-	}	
-    for (n = 0; nofrom[n]; n++)
-	if (strstr(sender, nofrom[n]))
-	{
-	    sprintf(genbuf, "From <%s> not accepted", sender);
-	    puts(genbuf);
-	    mailog(genbuf);
-	    return -1;
-	}	
-#endif
-
-    if ((fout = fopen(filename, "w")) == NULL)
-    {
+    
+    if((fout = fopen(filename, "w")) == NULL) {
 	printf("Cannot open %s\n", filename);
 	return -1;
     }
-
-    if (!title[0])
-	sprintf(title, "來自 %.64s", sender);
-    title[TTLEN] = 0;
-    fprintf(fout, "作者: %s\n標題: %s\n時間: %s\n",
-	    sender, title, ctime(&tmp_time));
-
-    while (fgets(genbuf, 255, stdin))
-    {
-#ifdef HMM_USE_ANTI_SPAM
-	for (n = 0; nocont[n]; n++)
-	    if (strstr(genbuf, nocont[n]))
-	    {
-		fclose(fout);
-		unlink(filename);
-		sprintf(genbuf, "Content <%s> not accepted", nocont[n]);
-		puts(genbuf);
-		mailog(genbuf);
-		return -1;
-	    }
-#endif
+    
+    if(title[0] == '\0')
+	snprintf(title, sizeof(title), "來自 %s", sender);
+    
+    if(sender[0])
+	fprintf(fout, "作者: %s\n", sender);
+    if(title[0])
+	fprintf(fout, "標題: %s\n", title);
+    if(msgid[0])
+	fprintf(fout, "\33[32m識別: %s\33[m\n", msgid);
+    fprintf(fout, "時間: %s\n", ctime(&tmp_time));
+    
+    while(fgets(genbuf, 255, stdin))
 	fputs(genbuf, fout);
-    }
     fclose(fout);
-
+    
     sprintf(genbuf, "%s => %s", sender, userid);
     mailog(genbuf);
-
-/* append the record to the MAIL control file */
-
+    
+    /* append the record to the MAIL control file */
+    
     strcpy(mymail.title, title);
-
-    if (strtok(sender, " .@\t\n\r"))
+    
+    if(strtok(sender, " .@\t\n\r"))
 	strcat(sender, ".");
     sender[IDLEN + 1] = '\0';
     strcpy(mymail.owner, sender);
-
+    
     sprintf(genbuf, BBSHOME "/home/%c/%s/.DIR", userid[0], userid);
     return append_record(genbuf, &mymail, sizeof(mymail));
 }
-
 
 int
 main(int argc, char* argv[])
